47c7a2bb7652f71a19a2d9a734a2880758cdd255
luckylinux <luckylinux>
2024-04-12 11:01:49 +0200

    Adding notes for further use

--- legacy/run_sync.sh
+++ legacy/run_sync.sh
@@ -26,3 +26,32 @@ skopeo inspect --raw docker://${DESTINATION_REGISTRY_HOSTNAME}/library/alpine | 
 
 # Or direct API call to registry ?
 #https://${DESTINATION_REGISTRY_HOSTNAME}/v2/docker.io/library/nginx/manifests/latest
+
+# Check if image is multi arch or not
+# https://stackoverflow.com/questions/75252938/is-there-a-way-to-check-if-a-manifest-in-a-docker-registry-is-multi-arch-or-not
+
+# List available tags for an image
+# https://${DESTINATION_REGISTRY_HOSTNAME}/v2/docker.io/library/nginx/tags/list
+
+# List available repositories
+# https://${DESTINATION_REGISTRY_HOSTNAME}/v2/_catalog
+
+# Some good Documentation
+# https://www.christopherbiscardi.com/post/building-a-docker-registry
+# https://distribution.github.io/distribution/spec/api/
+
+# Different types of manifests
+# https://distribution.github.io/distribution/spec/manifest-v2-2/
+
+
+# See also these notes:
+# https://stackoverflow.com/questions/57316115/get-manifest-of-a-public-docker-image-hosted-on-docker-hub-using-the-docker-regi
+
+
+# This seems to work out of the box
+./build/regclient/bin/regctl manifest get --host ${DESTINATION_REGISTRY_HOSTNAME} alpine:latest --format '{{jsonPretty .}}'
+./build/regclient/bin/regctl manifest get --host ${DESTINATION_REGISTRY_HOSTNAME} alpine@sha256:5d0da60400afb021f2d8dbfec8b7d26457e77eb8825cba90eba84319133f0efe --format '{{jsonPretty .}}'
+
+
+./build/regclient/bin/regctl manifest get --host ${DESTINATION_REGISTRY_HOSTNAME} alpine:latest --format '{{jsonPretty .}}'
+./build/regclient/bin/regctl manifest get --host ${DESTINATION_REGISTRY_HOSTNAME} alpine@sha256:5d0da60400afb021f2d8dbfec8b7d26457e77eb8825cba90eba84319133f0efe --format "{{printPretty .}}"
---
+++ test_local_mirror_bash_variant_2.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+# Source: https://stackoverflow.com/questions/57316115/get-manifest-of-a-public-docker-image-hosted-on-docker-hub-using-the-docker-regi
+
+# Determine toolpath if not set already
+relativepath="./" # Define relative path to go from this script to the root level of the tool
+if [[ ! -v toolpath ]]; then scriptpath=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd ); toolpath=$(realpath --canonicalize-missing $scriptpath/$relativepath); fi
+
+# Load configuration
+source $toolpath/.env
+
+# Define Function
+hub_manifest() {
+ref=${1}
+#sha="${ref#*@}"
+#if [ "$sha" = "$ref" ]; then
+#  sha=""
+#fi
+#wosha="${ref%%@*}"
+#repo="${wosha%:*}"
+repo=${ref}
+
+tag=${2}
+#if [ "$tag" = "$wosha" ]; then
+#  tag="latest"
+#fi
+
+
+
+#ref="${1:-library/ubuntu:latest}"
+#sha="${ref#*@}"
+#if [ "$sha" = "$ref" ]; then
+#  sha=""
+#fi
+#wosha="${ref%%@*}"
+#repo="${wosha%:*}"
+#tag="${wosha##*:}"
+#if [ "$tag" = "$wosha" ]; then
+#  tag="latest"
+#fi
+api="application/vnd.docker.distribution.manifest.v2+json"
+apil="application/vnd.docker.distribution.manifest.list.v2+json"
+#token=$(curl -s "https://auth.docker.io/token?service=registry.docker.io&scope=repository:${repo}:pull" \
+#        | jq -r '.token')
+curl -H "Accept: ${api}" -H "Accept: ${apil}" \
+     -H "Authorization: Bearer $token" \
+     -s "https://${DESTINATION_REGISTRY_HOSTNAME}/v2/${repo}/manifests/${sha:-$tag}" | jq .
+}
+
+# Define Counter
+counter=1
+
+# Loop
+while true
+do
+   timestamp=$(date +"%Y%m%d-%H:%M:%S")
+   manifest=$(hub_manifest "docker.io/library/nginx" "latest")
+
+   echo $manifest
+
+   #hash=$(echo "$digest" | grep "docker-content-digest" | sed -E "s|docker-content-digest:\s*?(.*)|\1|g")
+
+   #echo "$digest"
+   #echo "$hash"
+
+   #hash=$(./build/regclient/bin/regctl manifest head -p "linux/amd64" "docker.io/library/nginx:latest")
+   echo "Counter: ${counter} - Time: ${timestamp} - Hash: ${hash}"
+   #./bin/regctl version
+
+
+   sleep 2
+
+   # Increment Counter
+   counter=$((counter+1))
+done
+
